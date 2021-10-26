635c357c98db89945520ecce0e7db3c70c6d7477
ahmedgarip <rimo10yahoo@gmail.com>
2021-10-26 22:22:54 +0200

    Add files via upload

--- 03 - Sending Data to Templates/main.py
+++ 03 - Sending Data to Templates/main.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 
@@ -16,3 +15,12 @@ templates = Jinja2Templates(directory="templates")
 async def home_page(request: Request):
     return templates.TemplateResponse( "home.html", {"request": request})
 
+
+@app.get("/market", response_class=HTMLResponse)
+async def market_page(request: Request):
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return templates.TemplateResponse( "market.html", {"request": request, 'items':items})
